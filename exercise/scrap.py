d7cb75414b84c7cbdcbbdfc339ecca406d099967
root <root@debian.lan>
2015-10-13 19:13:44 +0200

    exercises added

---
+++ exercise/scrap.py
@@ -0,0 +1,2 @@
+if __name__ == '__main__':
+    print([elem for elem in range(1,10,2)])
---
+++ exercise/temperature.py
@@ -0,0 +1,14 @@
+import sys
+
+if __name__ == '__main__':
+    uno = lambda x: 1
+    due = lambda x: 2
+    tre = lambda x: 3
+    quattro = lambda x: 4
+    cinque = lambda x: 5
+    sei = lambda x: 6
+    sette = lambda x: 7
+    otto = lambda x: 8
+    temp_functions = [uno, due, tre, quattro, cinque, sei, sette, otto]
+    num = sys.argv[1]
+    
