8a659fb5ad00f3d8fa36d3c69adea6f09bffc517
Marco Odore <m.odore@gotosrl.it>
2015-12-20 16:45:22 +0100

    updated

--- exercise/Laboratorio1/temperatureconversionsystem.py
+++ exercise/Laboratorio1/temperatureconversionsystem.py
@@ -68,16 +68,39 @@ othersToCelsius = [celsiusToCelsius, fahrenheitToCelsius, kelvinToCelsius,
 temp = ["Celsius", "fahrenheit", "kelvin", "Rankine", "Delisle", "Newton",
         "Reaumer", "Romer"]
 
+scale = {"Celsius": 0, "Fahrenheit": 1, "Kelvin": 2, "Rankine": 3,
+         "Delisle": 4, "Newton": 5, "Reaumer": 6, "Romer": 7}
+
 
 def table(pureNumber):
-    for t in temp:
-        print("{:<13}".format(t), end="")
-    print("\n")
+    header = "".join(["{:<13}".format(t) for t in temp]) + "\n"
+    body = ""
+    partialbody = ""
     for x in othersToCelsius:
-        for y in celsiusToOthers:
-            print("{:<13.2f}".format(y(x(pureNumber))), end="")
-        print("\n")
+        partialbody = "".join(["{:<13.2f}".format(y(x(pureNumber)))
+                               for y in celsiusToOthers])
+        body = body + partialbody + "\n"
+    return header + body
+
+
+def toAll(temperature, scale_str):
+    numScale = scale[scale_str]
+    func = othersToCelsius[numScale]
+    conversion = [func(y(temperature)) for y in celsiusToOthers]
+    tempconversion = [(temp[i], conversion[i])for i in range(len(temp))]
+    tempconversion = sorted(tempconversion, key=lambda x: x[1])
+    body = "".join(["Type: {0:<10} Temp:{1:>10.2f}\n".format(x[0], x[1])
+                    for x in tempconversion])
+    return body
 
 
 if __name__ == '__main__':
-    table(10)
+    print(table(10))
+    print(toAll(10, "Celsius"))
+    print(toAll(10, "Fahrenheit"))
+    print(toAll(10, "Kelvin"))
+    print(toAll(10, "Rankine"))
+    print(toAll(10, "Delisle"))
+    print(toAll(10, "Newton"))
+    print(toAll(10, "Reaumer"))
+    print(toAll(10, "Romer"))
