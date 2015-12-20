b46be36e23380b269689a43c04d17ac2eaec3cd8
Marco Odore <m.odore@gotosrl.it>
2015-12-20 14:23:36 +0100

    completed

--- exercise/Laboratorio1/thecalendarmodule.py
+++ exercise/Laboratorio1/thecalendarmodule.py
@@ -1,7 +1,13 @@
 from calendar import isleap
+from calendar import weekday
+
+dayoftheweek = {0: "monday", 1: "tuesday", 2: "wednesday", 3: "thursday",
+                4: "friday", 5: "saturday", 6: "sunday"}
 
 if __name__ == '__main__':
     # anni bisestili
     print(isleap(2000))
     print(len(list(filter(isleap, [x for x in range(2000, 2051)]))))
     print(len([x for x in range(2000, 2051) if isleap(x)]))
+    # which day of the week July 20 2016 will be
+    print(dayoftheweek[weekday(2016, 7, 29)])
