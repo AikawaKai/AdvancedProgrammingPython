950f40680196b0c0213970790129bdb3cca24276
AikawaKai <m.odore@gotosrl.it>
2016-01-06 21:21:23 +0100

    socialnetwork

--- exercise/Laboratorio4/socialnetworks.py
+++ exercise/Laboratorio4/socialnetworks.py
@@ -14,6 +14,9 @@ class Nodo():
     def getValue(self):
         return self.__value
 
+    def getAdiacenti(self):
+        return self.__adiacenti
+
     def __str__(self):
         return "Nodo {0}, Adiacenti{1}".format(self.__value, [x.getValue() for x in self.__adiacenti])
 
@@ -22,11 +25,27 @@ class SocialNetwork():
 
     def __init__(self, firstNode):
         self._setOfNodes = set()
+        self._firstnode = firstNode
         self._setOfNodes.add(firstNode)
+
+    def addNode(self, node):
+        self._setOfNodes.add(node)
 
     def addAdiacenteToNode(self, node, adiacente):
         if node in self._setOfNodes:
             node.addAdiacente(adiacente)
+
+    def visitAll(self):
+        return [(elem.getValue(), [ad.getValue() for ad
+                                   in elem.getAdiacenti()])
+                for elem in self._setOfNodes]
+
+    def __str__(self):
+        lista = self.visitAll()
+        base = ""
+        for node, adiacenti in lista:
+            base += "\nNodo: {0}, Adiacenti{1}".format(node, adiacenti)
+        return base
 
 
 if __name__ == '__main__':
@@ -35,9 +54,14 @@ if __name__ == '__main__':
     nodo3 = Nodo(3, set())
     nodo4 = Nodo(4, set())
     setdiprova = {nodo4, nodo2, nodo3}
-    print(nodo1 in setdiprova)
+    # print(nodo1 in setdiprova)
     nodo1.addAdiacente(nodo2)
-    print(str(nodo1), str(nodo2))
+    # print(str(nodo1), str(nodo2))
     social = SocialNetwork(nodo1)
     social.addAdiacenteToNode(nodo1, nodo3)
-    print(str(nodo1), str(nodo3), str(nodo2))
+    social.addNode(nodo4)
+    social.addNode(nodo4)
+    social.addAdiacenteToNode(nodo4, nodo1)
+
+    #print(str(nodo1), str(nodo3), str(nodo2))
+    print(str(social))
