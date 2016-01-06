14c5500322568ea0e3f625e7d1499260cb78fd63
Marco Odore <m.odore@gotosrl.it>
2016-01-06 18:30:54 +0100

    added anagramOfStringFromDictionary

--- exercise/Laboratorio4/playingaroundwithstrings.py
+++ exercise/Laboratorio4/playingaroundwithstrings.py
@@ -30,6 +30,24 @@ class String(str):
                 return False
         return True
 
+    def subtract(self, other):
+        return ''.join([x for x in self if x not in other])
+
+    def anagram(self, other):
+        if len(self) != len(other):
+            return False
+        selfCount = {x: self.count(x) for x in set(self)}
+        otherCount = {x: other.count(x) for x in set(other)}
+        if selfCount == otherCount:
+            return True
+        else:
+            return False
+
+    def anagramOfStringFromDictionary(self, dictionary):
+        anagrams = [value for key, value in dictionary.items()
+                    if self.anagram(value)]
+        return anagrams
+
 if __name__ == '__main__':
     mystring = String("Do geese see God?")
     print(mystring.palindrom())  # True
@@ -50,3 +68,22 @@ if __name__ == '__main__':
     print(mystring.palindrom())  # False
     print(mystring.pythonicPalindrom())  # False
     print(mystring.iteratorPalindrom())  # False
+
+    mystring = String("Walter Cazzola")
+    toSubtract = String("abcwxyz")
+    print(mystring.subtract(toSubtract))
+
+    string1 = String("ciao")
+    string2 = String("caio")
+    string3 = String("caioooo")
+    string4 = String("aiocccco")
+    string5 = String("oiacoccc")
+    string6 = String("aioocccc")
+    string7 = String("aicoccco")
+    print(string1.anagram(string2))
+    print(string1.anagram(string3))
+    print(string1.anagram(string4))
+    print(string4.anagram(string5))
+    mydict = {0: string1, 1: string2, 2: string3, 3: string4, 4: string5,
+              5: string6}
+    print(string7.anagramOfStringFromDictionary(mydict))
