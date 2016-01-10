a6f637c1338a266398c5b099af6aa62ace797724
AikawaKai <marco.odore@gmail.com>
2016-01-10 22:09:41 +0100

    Delete recursivefact.py

--- exercise/recursivefact.py
+++
@@ -1,7 +0,0 @@
-
-def fact(x) :
-	return 1 if x == 1 else x * fact(x-1)
-
-if __name__ == '__main__' :
-	for i in [1, 2, 3, 4, 5, 6, 7, 15, 25, 30, 42, 100]:
-		print('Fattoriale di {0:3d} = {1}'.format(i,fact(i)))
