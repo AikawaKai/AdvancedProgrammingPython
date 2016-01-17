119b769c26cf4647d905b4fb3b68c2dca3e37aac
AikawaKai <m.odore@gotosrl.it>
2016-01-17 15:40:52 +0100

    pascal triangle testing done with next. Starting prev

--- exercise/Laboratorio5/pascaltriangle.py
+++ exercise/Laboratorio5/pascaltriangle.py
@@ -1,10 +1,50 @@
 class PascalTriangle(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, n):
+        self.count = 0
+        self.n = n
+        self.prevlist = []
 
     def __iter__(self):
-        pass
+        return self
 
     def __next__(self):
-        pass
+        if self.count >= self.n:
+            raise StopIteration
+        rowToNext = Row(self.prevlist, self.count)
+        rowToSave = Row(self.prevlist, self.count)
+        self.prevlist = [elem for elem in rowToSave]
+        self.count += 1
+        return rowToNext
+
+
+class Row(object):
+
+    def __init__(self, prevList, n):
+        self.prevList = prevList
+        self.n = n
+        self.count = 0
+        self.index1 = -1
+        self.index2 = 0
+
+    def __iter__(self):
+        return self
+
+    def _returnValueFromList(self, index):
+        if index < 0:
+            return 0
+        if index > len(self.prevList)-1:
+            return 0
+        return self.prevList[index]
+
+    def __next__(self):
+        if self.count > self.n:
+            raise StopIteration
+        if self.n == 0:
+            self.count += 1
+            return 1
+        valueToNext = self._returnValueFromList(self.index1) + self._returnValueFromList(self.index2)
+        self.index1 += 1
+        self.index2 += 1
+        self.count += 1
+        return valueToNext
--- exercise/Laboratorio5/testingpascaltriangle.py
+++ exercise/Laboratorio5/testingpascaltriangle.py
@@ -6,10 +6,13 @@ from pascaltriangle import *
 class TestingPascalTriangle(TestCase):
 
     def testPascal(self):
-        pascal = PascalTriangle()
+        pascal = PascalTriangle(5)
         row = next(pascal)
         self.assertEqual([1], [elem for elem in row])
-
-
+        self.assertEqual([1, 1], [elem for elem in next(pascal)])
+        self.assertEqual([1, 2, 1], [elem for elem in next(pascal)])
+        self.assertEqual([1, 3, 3, 1], [elem for elem in next(pascal)])
+        self.assertEqual([1, 4, 6, 4, 1], [elem for elem in next(pascal)])
+        self.assertRaises(StopIteration, next, pascal)
 if __name__ == '__main__':
     main()
