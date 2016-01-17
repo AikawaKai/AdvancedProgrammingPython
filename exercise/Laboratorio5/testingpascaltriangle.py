d79103dd8e5d2eb7985d2db305a421499cd370bd
AikawaKai <m.odore@gotosrl.it>
2016-01-17 16:26:08 +0100

    testing backward

--- exercise/Laboratorio5/testingpascaltriangle.py
+++ exercise/Laboratorio5/testingpascaltriangle.py
@@ -16,8 +16,20 @@ class TestingPascalTriangle(TestCase):
         self.assertRaises(StopIteration, next, pascal)
 
     def testPrev(self):
-        pass
-
+        pascal = PascalTriangle(5)
+        row = next(pascal)
+        self.assertEqual([1], [elem for elem in row])
+        self.assertEqual([1, 1], [elem for elem in next(pascal)])
+        self.assertEqual([1, 2, 1], [elem for elem in next(pascal)])
+        self.assertEqual([1, 3, 3, 1], [elem for elem in next(pascal)])
+        self.assertEqual([1, 4, 6, 4, 1], [elem for elem in next(pascal)])
+        self.assertEqual([1, 4, 6, 4, 1], [elem for elem in pascal.prev()])
+        self.assertEqual([1, 3, 3, 1], [elem for elem in pascal.prev()])
+        self.assertEqual([1, 2, 1], [elem for elem in pascal.prev()])
+        self.assertEqual([1, 1], [elem for elem in pascal.prev()])
+        self.assertEqual([1], [elem for elem in pascal.prev()])
+        self.assertEqual([], [elem for elem in pascal.prev()])
+        self.assertRaises(StopIteration, pascal.prev)
 
 if __name__ == '__main__':
     main()
