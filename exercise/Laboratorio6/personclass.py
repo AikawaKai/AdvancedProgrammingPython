f32eaa8e95823fe41720025dd02cd75709a4a85b
AikawaKai <m.odore@gotosrl.it>
2016-01-17 17:20:51 +0100

    Student updated

--- exercise/Laboratorio6/personclass.py
+++ exercise/Laboratorio6/personclass.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+
 class Person(object):
 
     def __init__(self, name, lastname, birthday):
@@ -22,3 +25,32 @@ class Person(object):
 
     def setBirthday(self, newbirthday):
         self.birthday = newbirthday
+
+
+class Student(Person):
+
+    lectures = dict()
+
+    def __init__(self, name, lastname, birthday, LectMarkTuples):
+        super(Student, self).__init__(name, lastname, birthday)
+        for lect, mark in LectMarkTuples:
+            self.lectures[lect] = mark
+
+    def _getAverageGrade(self):
+        summark = 0
+        for lect, mark in self.lectures.items():
+            summark += mark
+        return summark / len(self.lectures)
+
+    grade_average = property(_getAverageGrade, None, None, "Average grade")
+
+
+if __name__ == '__main__':
+    lectmark = [("Matematica1", 24), ("Matematica2", 27), ("Statistica", 30),
+                ("Programmazione", 27), ("Sistemi Operativi", 27),
+                ("Tecnocivismo", 28), ("Basi di dati", 23),
+                ("Progr. funzionale", 25), ("ArchitetturaI", 25),
+                ("ArchitetturaII", 24), ("Crittografia", 30),
+                ("Linguaggi formali", 25), ("Prog. Software", 24)]
+    student = Student("Marco", "Odore", date(1985, 10, 27), lectmark)
+    print(student.grade_average)
