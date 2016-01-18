60fcc74a1ff8ef3118c7a5e42e5a7bc95e6cb6f7
AikawaKai <m.odore@gotosrl.it>
2016-01-18 18:33:06 +0100

    personclass.py

--- exercise/Laboratorio6/decoratorsonmath.py
+++ exercise/Laboratorio6/decoratorsonmath.py
@@ -3,8 +3,12 @@ class MyMath(object):
     def fib(n):
         return 1 if n <= 1 else MyMath.fib(n-1) + MyMath.fib(n-2)
 
+    def fact(n):
+        return 1 if n <= 1 else n * MyMath.fact(n-1)
+
 
 if __name__ == '__main__':
+    #  fibonacci series
     print(MyMath.fib(0))
     print(MyMath.fib(1))
     print(MyMath.fib(2))
@@ -13,3 +17,13 @@ if __name__ == '__main__':
     print(MyMath.fib(5))
     print(MyMath.fib(6))
     print(MyMath.fib(7))
+
+    #  factorial
+    print(MyMath.fact(0))
+    print(MyMath.fact(1))
+    print(MyMath.fact(2))
+    print(MyMath.fact(3))
+    print(MyMath.fact(4))
+    print(MyMath.fact(5))
+    print(MyMath.fact(6))
+    print(MyMath.fact(7))
