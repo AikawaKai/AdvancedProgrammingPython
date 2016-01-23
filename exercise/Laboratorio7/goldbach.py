039909bc317c01d1b4595ac349e9e4b63663ad15
AikawaKai <m.odore@gotosrl.it>
2016-01-23 15:29:09 +0100

    update prime generator

--- exercise/Laboratorio7/goldbach.py
+++ exercise/Laboratorio7/goldbach.py
@@ -14,6 +14,41 @@ def isPrime(n):
     return True
 
 
+class GeneratePrime(object):
+
+    def __init__(self, maximum):
+        self.number = 0
+        self.index = 0
+        self.cache = []
+        self.maximum = maximum
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def getCache(self):
+        return self.cache
+
+    def __next__(self):
+        if self.number > self.maximum:
+            raise StopIteration
+        if len(self.cache) > self.index:
+            toReturn = self.cache[self.index]
+            self.index += 1
+            return toReturn
+        while(not isPrime(self.number)):
+            self.number += 1
+        toReturn = self.number
+        self.cache.append(toReturn)
+        self.number += 1
+        self.index += 1
+        return toReturn
+
+
 if __name__ == '__main__':
     print(list(filter(isPrime, [elem for elem in range(0, 100)])))
-    print(isPrime(9))
+    iterator = GeneratePrime(100)
+    print([elem for elem in iterator])
+    print("la cache 1° iterazione", iterator.getCache())
+    print([elem for elem in iterator])
+    print("la cache 2° iterazione", iterator.getCache())
