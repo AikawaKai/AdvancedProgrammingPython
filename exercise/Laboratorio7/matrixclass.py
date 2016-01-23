59d20b90a07d37e76ff07be70e459960272e668a
AikawaKai <m.odore@gotosrl.it>
2016-01-23 17:55:40 +0100

    to fix major bug

--- exercise/Laboratorio7/matrixclass.py
+++ exercise/Laboratorio7/matrixclass.py
@@ -22,16 +22,16 @@ class Matrix(object):
     def getMatrix(self):
         return self.matrix
 
-    def getLenRow(self):
+    def getNumRow(self):
         return self.n
 
-    def getLenColumn(self):
+    def getNumColumn(self):
         return self.m
 
     def __add__(self, other):
-        if self.getLenRow() == other.getLenRow() and self.getLenColumn() == other.getLenColumn():
+        if self.getNumRow() == other.getNumRow() and self.getNumColumn() == other.getNumColumn():
             otherM = other.getMatrix()
-            matrix = [[self.matrix[i][j] + otherM[i][j] for j in range(self.getLenColumn())] for i in range(self.getLenRow())]
+            matrix = [[self.matrix[i][j] + otherM[i][j] for j in range(self.getNumColumn())] for i in range(self.getNumRow())]
             return Matrix(matrix)
         raise ValueError("Matrix are not compatible for the sum operation")
 
@@ -43,7 +43,23 @@ class Matrix(object):
         raise ValueError("Stai moltiplicando per un valore non concesso")
 
     def _mul(self, other):
-        return [[self.matrix[i][j] * other for j in range(self.getLenColumn())] for i in range(self.getLenRow())]
+        return [[self.matrix[i][j] * other for j in range(self.getNumColumn())] for i in range(self.getNumRow())]
+
+    def _mulM(self, matrix):
+        if not self.getNumColumn() == matrix.getNumRow():
+            raise ValueError("Operazione non concessa")
+        matrixO = matrix.getMatrix()
+        gC = Matrix._getColumn
+        mV = Matrix._mulTwoVectors
+        return Matrix([mV(self.matrix[i], gC(matrixO, j)) for j in
+                      range(self.getNumColumn()) for i in
+                      range(self.getNumRow())])
+
+    def _getColumn(matrix, j):
+        return [matrix[i][j] for i in range(len(matrix))]
+
+    def _mulTwoVectors(vect1, vect2):
+        return sum([vect1[i]*vect2[i] for i in range(len(vect1))])
 
     def __eq__(self, othermatrix):
         return True if self.matrix == othermatrix.getMatrix() else False
@@ -74,3 +90,6 @@ if __name__ == '__main__':
     print(matrix3.getMatrix())  # reference to matrix
     print((matrix3 + matrix1).getMatrix())
     print(matrix1 * 3)
+    print(matrix1 * matrix3)
+    #  print(Matrix.getColumn(matrix1.getMatrix(), 0))
+    #  print(matrix1 * 7.5)
