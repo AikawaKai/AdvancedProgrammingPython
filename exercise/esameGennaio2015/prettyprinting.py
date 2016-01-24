e7a4ad408d73107be9b51f1c50bd0af86cfd2e4b
AikawaKai <m.odore@gotosrl.it>
2016-01-24 13:21:51 +0100

    first attempt ok

---
+++ exercise/esameGennaio2015/prettyprinting.py
@@ -0,0 +1,18 @@
+from functools import reduce
+
+
+def prettyCSV(file):
+    maxM = lambda x, y: y if len(y) > len(x) else x
+    sumM = lambda x, y: x + "\n" + y
+    output = open(file, 'r')
+    lines = [i.split(";") for i in output]
+    regroup = [[lines[i][j] for i in range(len(lines))] for j in range(len(lines[0]))]
+    maxSizeColumns = [len(reduce(maxM, regroup[i])) for i in range(len(regroup))]
+    print(regroup, maxSizeColumns)
+    newRegroup = [["{0:<{1}}".format(regroup[j][i], maxSizeColumns[j]) for j in range(len(regroup))] for i in range(len(regroup[0]))]
+    newRegroup = [[newRegroup[i][j] for j in range(len(newRegroup[0]))] for i in range(len(newRegroup))]
+    print("\n".join(["|".join(elem) for elem in newRegroup]))
+
+
+if __name__ == '__main__':
+    prettyCSV("books.csv")
