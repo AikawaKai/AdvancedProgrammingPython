211540f9adbe498813916965a454146ac98dee3f
AikawaKai <m.odore@gotosrl.it>
2016-01-24 16:38:58 +0100

    updated set generation

--- exercise/esameFebbraio2015/testingring.py
+++ exercise/esameFebbraio2015/testingring.py
@@ -8,7 +8,7 @@ class Z(object):
     def __init__(self, maxN):
         self.max = maxN * 2
         self.count = 1
-        self.posNeg = [1, -1]
+        self.posNeg = [-1, 1]
         self.cache = [0]
         self.index = 0
 
@@ -17,8 +17,12 @@ class Z(object):
         return self
 
     def __next__(self):
-        if self.count > (self.max * 2) + 1:
+        if self.count > self.max:
             raise StopIteration
+        if self.index < len(self.cache):
+            toReturn = self.cache[self.index]
+            self.index += 1
+            return toReturn
         toExtend = [s * self.count for s in self.posNeg]
         self.cache.extend(toExtend)
         toReturn = self.cache[self.index]
@@ -40,7 +44,9 @@ class TestinRing(TestCase):
     def testInitClass(self):
         z = Z(100)
         set1 = set(z)
-        print(set1)
+        z1 = Z(100)
+        set2 = set(z1)
+        print(set1, set2)
         ring = Ring(set1, op1, op2)
 
 
