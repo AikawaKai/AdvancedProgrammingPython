70363f789abf9a22e7534aa9b6901c4718de7b4e
AikawaKai <m.odore@gotosrl.it>
2016-01-24 15:36:20 +0100

    sort of completed

---
+++ exercise/esameGennaio2015/skinObject.py
@@ -0,0 +1,43 @@
+
+def become(self, set1, set2):
+    for fun in set1:
+        self.__dict__[fun.__name__] = fun
+    for fun in set2:
+        if fun.__name__ in self.__dict__:
+            del self.__dict__[fun.__name__]
+
+
+def pop(self):
+    self.top -= 1
+    toReturn = self.data[self.top]
+    del self.data[self.top]
+    return toReturn
+
+
+def push(self, elem):
+    self.data.append(elem)
+    self.top += 1
+
+
+class Skin(type):
+
+    def __new__(meta, classname, supers, classdict):
+        classdict["become"] = become
+        return type.__new__(meta, classname, supers, classdict)
+
+
+class stack(metaclass=Skin):
+
+    def __init__(self, dim=10):
+        self.dimension = dim
+        self.top = 0
+        self.data = []
+
+    def is_empty(self):
+        return self.top == 0
+
+    def is_full(self):
+        return self.top == (self.dimension-1)
+
+    def __str__(self):
+        return "Stack top :- {0} Stack dim :- {1} Stack data :- {2}".format(self.top, self.dimension, self.data)
---
+++ exercise/esameGennaio2015/testingskins.py
@@ -0,0 +1,14 @@
+from skinObject import *
+
+if __name__ == "__main__":
+    s = stack(5)  # 5 is the stack dimension
+    trend = True
+    for i in range(-1, 14):
+        if s.is_empty():
+            s.become({push}, {pop})
+            trend = True
+        elif s.is_full():
+            s.become({pop}, {push})
+            trend = False
+        push(s, i) if trend else pop(s)
+        print(s)
