5a4f3c5c81ddcf90a2e48ddc244cebcbded937f9
AikawaKai <m.odore@gotosrl.it>
2016-01-24 13:42:50 +0100

    completed

--- exercise/esameGennaio2015/prettyprinting.py
+++ exercise/esameGennaio2015/prettyprinting.py
@@ -4,6 +4,7 @@ from functools import reduce
 def cleaning(string):
     string = string.replace("\n", "")
     string = string.replace('""', "")
+    string = string.replace('"', "")
     return " "+" ".join(string.split())+" "
 
 
@@ -14,10 +15,12 @@ def prettyCSV(file):
     regroup = [[cleaning(lines[i][j]) for i in range(len(lines))] for j in range(len(lines[0]))]
     maxSizeColumns = [len(reduce(maxM, regroup[i])) for i in range(len(regroup))]
     print(regroup, maxSizeColumns)
+    sizeLine = sum(maxSizeColumns)+len(regroup)+1
+    stringLine = "{0:-^{1}}".format("", sizeLine)
     newRegroup = [["{0:<{1}}".format(regroup[j][i], maxSizeColumns[j]) for j in range(len(regroup))] for i in range(len(regroup[0]))]
     newRegroup = [[newRegroup[i][j] for j in range(len(newRegroup[0]))] for i in range(len(newRegroup))]
-    print("\n".join(["|"+"|".join(elem)+"|" for elem in newRegroup]))
-
+    returnstring = "\n".join(["|"+"|".join(newRegroup[i])+"|" if i != 1 else stringLine for i in range(len(newRegroup))])
+    return stringLine+"\n"+returnstring+"\n"+stringLine
 
 if __name__ == '__main__':
-    prettyCSV("books.csv")
+    print(prettyCSV("languages.csv"))
