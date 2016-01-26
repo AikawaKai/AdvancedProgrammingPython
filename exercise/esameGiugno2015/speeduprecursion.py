d6c175114cd167524ae9ee15f88d874020729900
AikawaKai <m.odore@gotosrl.it>
2016-01-26 16:35:35 +0100

    updated

--- exercise/esameGiugno2015/speeduprecursion.py
+++ exercise/esameGiugno2015/speeduprecursion.py
@@ -18,11 +18,7 @@ def fact(n):
 
 @memoization
 def fibo(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fibo(n-1) + fibo(n-2)
+    return n if n <= 1 else fibo(n-1) + fibo(n-2)
 
 
 @memoization
