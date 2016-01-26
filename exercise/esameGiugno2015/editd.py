743cfd35a77e38b79a31a6cd1df9c6d030c5d42d
AikawaKai <m.odore@gotosrl.it>
2016-01-26 15:46:20 +0100

    updated

--- exercise/esameGiugno2015/editd.py
+++ exercise/esameGiugno2015/editd.py
@@ -5,23 +5,30 @@ def diff1(string1, string2):
     return True if sum([0 if list(string1)[i] == list(string2)[i] else 1 for i in range(len(string1))]) == 1 else False
 
 listOfWords = [word for word in open("dizionario.txt", 'r').read().split()]
+print(listOfWords)
 
 
 def chain(string1, string2):
-    return chainr(string1, string2, listOfWords, [], [])
+    listofmatch = chainr(string1, string2, listOfWords, [], [])
+    string = ""
+    for array in listofmatch:
+        string += str(array)+"\n"
+    return string
+
+
 
 
 def chainr(string1, string2, listOfWords, lista, listOfList):
     if string1 == string2:
         lista.append(string1)
-        return lista
+        listOfList.append(lista)
     listOfNext = [elem for elem in listOfWords if diff1(elem, string1)]
     for nextElem in listOfNext:
         newlist = copy.deepcopy(lista)
         newlist.append(string1)
         lWCp = copy.deepcopy(listOfWords)
         lWCp.remove(string1)
-        listOfList.append(chainr(nextElem, string2, lWCp, newlist, listOfList))
+        chainr(nextElem, string2, lWCp, newlist, listOfList)
     return listOfList
 
 
@@ -32,5 +39,10 @@ if __name__ == '__main__':
     print(diff1("ziao", "ciaa"))
     print(diff1("ziao", "ciao"))
     print(diff1("ziao", "ciao"))
+    print("### witness → fatness")
     print(chain("witness", "fatness"))
+    print("### warning → earring")
     print(chain("warning", "earring"))
+    print("### sailing → writing")
+    print(chain("sailing", "writing"))
+    # print(lista2)
