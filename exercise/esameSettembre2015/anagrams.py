d66bf8476c8f0d7fbd394979a27eb9054e6ee03f
AikawaKai <m.odore@gotosrl.it>
2016-02-13 22:03:21 +0100

    my anagrams

---
+++ exercise/esameSettembre2015/anagrams.py
@@ -0,0 +1,25 @@
+dictionary = {}
+
+
+def anagrams():
+    fileopen = open("wordlist-anagram.txt")
+    lines = [line.strip() for line in fileopen]
+    for line in lines:
+        sortedline = "".join(sorted(list(line.lower())))
+        if sortedline in dictionary:
+            if line not in dictionary[sortedline]:
+                dictionary[sortedline].append(line)
+        else:
+            dictionary[sortedline] = [line]
+    new_dict = list(filter(lambda x: len(x[1]) > 2, dictionary.items()))
+    new_dict = {lista[0]: ", ".join(lista[1:]) for word, lista in new_dict}
+    new_dict = sorted(new_dict.items(), key=lambda x: x[0])
+
+    print(len(new_dict))
+    base = ""
+    for key, value in new_dict:
+        base += "{0:<15}:- {1}\n".format(key, value)
+    print(base)
+
+if __name__ == '__main__':
+    print(anagrams())
