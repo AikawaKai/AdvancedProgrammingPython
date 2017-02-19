9541c8b3ddaba8ae28e25271ccef6a08292beaf0
Marco <marco.odore@gmail.com>
2017-02-19 15:43:21 +0100

    callgraph first attempt

--- TemiDesame/callgraphs/dot.py
+++ TemiDesame/callgraphs/dot.py
@@ -1,10 +1,35 @@
 import ABC
 from types import *
+import sys
+import inspect
+
+
+file_o = open("./cg.dot", 'w')
+file_o.write("strict digraph cg {\n")
+
+file_o.close()
 
 def decor(fun):
     def wrapper(*args, **kargs):
-        print(fun.__name__, " ")
         res = fun(*args, **kargs)
+        f = sys._getframe()
+        if fun.__name__ == 'cc':
+            args = "("+str(inspect.getargvalues(f)[3]['args'][0])+")"
+            list_ = [fun.__name__+args]
+            while True:
+                if f.f_back.f_back is not None:
+                    curr_fun = inspect.getframeinfo(f).function
+                    if(curr_fun != 'wrapper'):
+                        args = "("+str(inspect.getargvalues(f)[3]['i'])+")"
+                        list_.append(curr_fun+args+" -> ")
+                    f = f.f_back
+                else:
+                    curr_fun = inspect.getframeinfo(f).function
+                    list_.append('main ->')
+                    break;
+            file_o = open("./cg.dot", 'a')
+            file_o.write("\t"+("".join(list_[::-1])+"\n"))
+            file_o.close()
         return res
     return wrapper
 
