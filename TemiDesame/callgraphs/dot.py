77498ddf256406fcf39b3300b75adead53166380
Marco <marco.odore@gmail.com>
2017-02-19 16:54:50 +0100

    my solution almost complete

--- TemiDesame/callgraphs/dot.py
+++ TemiDesame/callgraphs/dot.py
@@ -7,21 +7,30 @@ import inspect
 
 file_o = open("./cg.dot", 'w')
 file_o.write("strict digraph cg {\n")
-
 file_o.close()
-print(main)
 
+classes = [(key, value) for key, value in ABC.__dict__.items() if type(value) is type(object)]
+dict_classes = {key:[val.__name__ for val in value.__dict__.values() if type(val) is FunctionType] for key, value in classes}
 def decor(fun):
     def wrapper(*args, **kargs):
         res = fun(*args, **kargs)
         f = sys._getframe()
         if fun.__name__ == 'cc':
+            curr_fun = fun.__name__
             args = "("+str(inspect.getargvalues(f)[3]['args'][0])+")"
-            list_ = [fun.__name__+args]
+            for key, value in dict_classes.items():
+                if curr_fun in value:
+                    curr_fun = key+"."+curr_fun
+                    break;
+            list_ = [curr_fun+args]
             while True:
                 if f.f_back.f_back is not None:
                     curr_fun = inspect.getframeinfo(f).function
                     if(curr_fun != 'wrapper'):
+                        for key, value in dict_classes.items():
+                            if curr_fun in value:
+                                curr_fun = key+"."+curr_fun
+                                break;
                         args = "("+str(inspect.getargvalues(f)[3]['i'])+")"
                         list_.append(curr_fun+args+" -> ")
                     f = f.f_back
@@ -40,6 +49,7 @@ class MetaPath(type):
     def __new__(self, classname, supers, dict_, **kwds):
         for key, value in dict_.items():
             if type(value) is FunctionType:
+                value.classname = "classname"
                 dict_[key]=decor(value)
         return type.__new__(self, classname, supers, dict_, **kwds)
 
