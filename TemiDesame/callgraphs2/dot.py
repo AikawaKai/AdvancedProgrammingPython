018327ff786d650a66c312a0144fcd8b2cd36980
Marco <marco.odore@gmail.com>
2017-02-23 15:20:19 +0100

    first attempt

--- TemiDesame/callgraphs2/dot.py
+++ TemiDesame/callgraphs2/dot.py
@@ -1,9 +1,22 @@
 from ABC import *
+from types import FunctionType
+
+def decor(fun):
+    def wrapper(*args):
+        file_o = open("./cg.dot", "a")
+        file_o.write(fun.__name__+str(args)+" -> ")
+        file_o.close()
+        res = fun(*args)
+        return res
+    return wrapper
 
 class CG(type):
 
     def __init__(self, classname, supers, dict_, *others):
         print(classname)
+        for key, fun in dict_.items():
+            if type(fun) is FunctionType:
+                dict_[key] = decor(fun)
         return type.__init__(self, classname, supers, dict_, *others)
 
 
